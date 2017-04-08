00c37d2a200c5be0600dca09f4694d9052cc96fe
GlisseLisbeth <glisselisbeth@gmail.com>
2017-04-08 00:18:22 -0500

    Finish

--- assets/js/typer.js
+++ assets/js/typer.js
@@ -1,56 +1,70 @@
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
+function setupTypewriter(t) {
+       var HTML = t.innerHTML;
 
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
+       t.innerHTML = "";
 
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
+       var cursorPosition = 0,
+           tag = "",
+           writingTag = false,
+           tagOpen = false,
+           typeSpeed = 100,
+       tempTypeSpeed = 0;
 
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+       var type = function() {
 
-        var that = this;
-        var delta = 200 - Math.random() * 100;
+           if (writingTag === true) {
+               tag += HTML[cursorPosition];
+           }
 
-        if (this.isDeleting) { delta /= 2; }
+           if (HTML[cursorPosition] === "<") {
+               tempTypeSpeed = 0;
+               if (tagOpen) {
+                   tagOpen = false;
+                   writingTag = true;
+               } else {
+                   tag = "";
+                   tagOpen = true;
+                   writingTag = true;
+                   tag += HTML[cursorPosition];
+               }
+           }
+           if (!writingTag && tagOpen) {
+               tag.innerHTML += HTML[cursorPosition];
+           }
+           if (!writingTag && !tagOpen) {
+               if (HTML[cursorPosition] === " ") {
+                   tempTypeSpeed = 0;
+               }
+               else {
+                   tempTypeSpeed = (Math.random() * typeSpeed) + 10;
+               }
+               t.innerHTML += HTML[cursorPosition];
+           }
+           if (writingTag === true && HTML[cursorPosition] === ">") {
+               tempTypeSpeed = (Math.random() * typeSpeed) + 10;
+               writingTag = false;
+               if (tagOpen) {
+                   var newSpan = document.createElement("span");
+                   t.appendChild(newSpan);
+                   newSpan.innerHTML = tag;
+                   tag = newSpan.firstChild;
+               }
+           }
 
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
+           cursorPosition += 1;
+           if (cursorPosition < HTML.length - 1) {
+               setTimeout(type, tempTypeSpeed);
+           }
 
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
+       };
 
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
+       return {
+           type: type
+       };
+   }
+
+   var typer = document.getElementById('typewriter');
+
+   typewriter = setupTypewriter(typewriter);
+
+   typewriter.type();
